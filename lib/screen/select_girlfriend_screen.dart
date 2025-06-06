6ca91a80d579ea7fa703c57481e988958f9d44f6
kuroda50 <144975768+kuroda50@users.noreply.github.com>
2025-06-06 18:48:40 +0900

    go_routerの設定を途中までやった

---
+++ lib/screen/select_girlfriend_screen.dart
@@ -0,0 +1,12 @@
+/* 彼女選択画面 */
+
+import 'package:flutter/material.dart';
+
+class SelectGirlfriendScreen extends StatelessWidget {
+  const SelectGirlfriendScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
---
+++ lib/screen/select_story_screen.dart
@@ -0,0 +1,12 @@
+/* ストーリー選択画面 */
+
+import 'package:flutter/material.dart';
+
+class SelectStoryScreen extends StatelessWidget {
+  const SelectStoryScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
