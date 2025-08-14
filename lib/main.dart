0a40e5abe361ade1d936d064209b8b9c555c301c
kuroda50 <144975768+kuroda50@users.noreply.github.com>
2025-08-14 23:07:42 +0900

    LocalStorageのインスタンスを取得する処理を一か所にまとめた

--- lib/main.dart
+++ lib/main.dart
@@ -2,12 +2,16 @@ import 'package:flutter/material.dart';
 import 'package:device_preview/device_preview.dart';
 import 'package:flutter/foundation.dart';
 import 'package:saving_girlfriend/route/go_router.dart';
+import 'services/local_storage_service.dart';
 
-void main() {
-  runApp(  DevicePreview(
-    enabled: !kReleaseMode,
-    builder: (context) => MyApp(), // Wrap your app
-  ),);
+void main() async {
+  await LocalStorageService.init();
+  runApp(
+    DevicePreview(
+      enabled: !kReleaseMode,
+      builder: (context) => MyApp(), // Wrap your app
+    ),
+  );
 }
 
 class MyApp extends StatelessWidget {
