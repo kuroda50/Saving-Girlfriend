735a419e314f5a94ddc3a8b6a84cee70b645ebac
kuroda50 <144975768+kuroda50@users.noreply.github.com>
2025-08-17 07:42:46 +0900

    deployが不要なことがわかったので中断

--- lib/main.dart
+++ lib/main.dart
@@ -4,9 +4,16 @@ import 'package:flutter/foundation.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:saving_girlfriend/route/go_router.dart';
 import 'services/local_storage_service.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
 
 void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
   await LocalStorageService.init();
+  // Firebaseを初期化
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(
     DevicePreview(
       enabled: !kReleaseMode,
