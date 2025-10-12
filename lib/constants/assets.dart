0afebd0a6846186bc985683302d67511223ecb47
shiomizu0620 <akubi0302@icloud.com>
2025-10-12 21:16:19 +0900

    スーパーチャットが定型文のみに反応する仕組みを作成

--- lib/constants/assets.dart
+++ lib/constants/assets.dart
@@ -16,6 +16,17 @@ class AppAssets {
   static const String iconLockOpen = '$_icon/lock_open.png';
   static const String iconEn = '$_icon/en.png';
 
+  static const String iconPlayer = '$_icon/player.png';
+
+  // --- ダミー視聴者アイコン ---
+  static const String iconUser2 = '$_icon/user2.png.jpg';
+  static const String iconUser3 = '$_icon/user3.png.jpg';
+  static const String iconUser4 = '$_icon/user4.png.jpg';
+  static const String iconUser5 = '$_icon/user5.png.png';
+  static const String iconUser6 = '$_icon/user6.png.jpg';
+  static const String iconUser7 = '$_icon/user7.png.jpg';
+
+
   /// BGMへのベースパス
   static const String _bgm = 'assets/bgm';
 
