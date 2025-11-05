41e03eecf02a97dfac661dc958333414a1be7444
kuroda50 <144975768+kuroda50@users.noreply.github.com>
2025-11-05 15:31:07 +0900

    タイポを修正

--- lib/screen/home_screen.dart
+++ lib/screen/home_screen.dart
@@ -5,7 +5,7 @@ import 'dart:math';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:saving_girlfriend/constants/assets.dart';
-import 'package:saving_girlfriend/date/scenario_data.dart';
+import 'package:saving_girlfriend/data/scenario_data.dart';
 import 'package:saving_girlfriend/models/comment_model.dart';
 import 'package:saving_girlfriend/widgets/super_chat_modal.dart';
 
